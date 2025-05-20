2b44b5e13afa5fdaf845605137859ab98755a757
Rimanagi <rim.linking@mail.ru>
2025-05-20 16:31:45 +0300

    добавлена ссылка на GitHub Pages

--- backend/bot/keyboards/inline.py
+++ backend/bot/keyboards/inline.py
@@ -1,10 +1,9 @@
 from aiogram.utils.keyboard import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.types import WebAppInfo
 
-from backend.config import URL
-
+from backend.config import HTTPS_URL
 
 welcome_keyboard = [
-    [InlineKeyboardButton(text='Старт', web_app=WebAppInfo(url='https://google.com'))],
+    [InlineKeyboardButton(text='Старт', web_app=WebAppInfo(url=HTTPS_URL))],
 ]
 get_welcome_keyboard = InlineKeyboardMarkup(inline_keyboard=welcome_keyboard)
