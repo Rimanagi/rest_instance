7e44de3d3103815e3cd26ebd98b866c229456c08
Павел Коноплев <pavel.konoplev@softcom.su>
2025-05-21 23:21:28 +0300

    add vue frontend

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,7 @@ from contextlib import asynccontextmanager
 
 import uvicorn
 from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.templating import Jinja2Templates
 from backend.bot.app_tg import run_bot, stop_bot
 
@@ -17,12 +18,25 @@ async def lifespan(app: FastAPI):
     bot_task.cancel()
 
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI()
+
+# Настройка CORS - разрешаем все origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Разрешаем все домены
+    allow_credentials=True,
+    allow_methods=["*"],  # Разрешаем все HTTP-методы
+    allow_headers=["*"],  # Разрешаем все заголовки
+)
 
 templates = Jinja2Templates(directory="docs")
-@app.get("/")
+
+@app.get("/list")
 async def ping(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request, "host": HOST, "port": PORT})
+    return { 
+        'port': 8080,
+        'address': '0.0.0.0'
+    }
 
 
 if __name__ == '__main__':
